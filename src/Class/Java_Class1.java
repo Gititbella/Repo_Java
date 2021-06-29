1384f66589e54c0706ef97d32b3247f44a066ddf
Isabella Siasoco <bellabelike@Isabellas-MacBook-Pro.local>
2021-06-28 17:08:17 -0700

    addming my first commit

---
+++ src/Class/Java_Class1.java
@@ -0,0 +1,31 @@
+package Class;
+
+public class Java_Class1 {
+
+    // main method
+    public static void main(String[] args) {
+        /** no main method, cannot run file
+         * starting point of any Java execution
+         *
+         * shortcut to create main method psvm (only in IntelliJ)
+         *
+         * In a class, we can only have ONE main method
+         */
+
+        /**
+         * Library: multiple packages and/or files
+         *
+         * Package: folder
+         *
+         * Class: file
+         */
+
+        System.out.println("Hello World!");
+        System.out.println("Have a great day!");
+
+
+        /**
+         * Console: System.out.println prints the result in the console window.
+         */
+    }
+}
